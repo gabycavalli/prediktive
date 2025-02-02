34a73c4916945528dbe8e5949c62d2ac972a0d60
g.cavalli <gabrielcavalli@gmail.com>
2025-02-01 21:01:10 -0300

    adding-gitignore-reports

--- cypress.config.ts
+++ cypress.config.ts
@@ -1,17 +1,3 @@
-//const { defineConfig } = require('cypress');
-
-// module.exports = defineConfig({
-//   e2e: {
-//     setupNodeEvents(on, config) {
-      
-//       // implement node event listeners here
-//     },
-//     specPattern: 'cypress/e2e/**/*.cy.{js,jsx,ts,tsx}',
-//     supportFile: 'cypress/support/commands.ts',
-//   },
-// });
-
-// cypress.config.ts
 import { defineConfig } from 'cypress';
 import webpackPreprocessor from '@cypress/webpack-preprocessor';
 import webpackConfig from './cypress/webpack.config';
@@ -20,10 +6,11 @@ export default defineConfig({
   e2e: {
      reporter: 'mochawesome',
     reporterOptions: {
-      reportDir: 'cypress/reports', // Directorio donde se almacenarán los reportes JSON
-      overwrite: false, // No sobrescribir reportes anteriores
-      html: true, // Generar el reporte en formato HTML
-      json: true, // Generar el reporte en formato JSON
+      reportDir: 'cypress/reports',
+      overwrite: false,
+      video: true,
+      html: true,
+      json: true,
     },
     setupNodeEvents(on, config) {
       const options = {
--- run_cli_test.sh
+++ run_cli_test.sh
@@ -1,26 +1,26 @@
 #!/bin/bash
 
-# Ruta de la carpeta de reportes
+# Report directory
 REPORT_DIR="cypress/reports"
 
-# Verificar si la carpeta de reportes ya existe y borrarla
+# If the report directory exists, remove it
 if [ -d "$REPORT_DIR" ]; then
     echo "Removing existing report directory..."
     rm -rf "$REPORT_DIR"
 fi
 
-# Crear la carpeta de reportes (si no existe)
+# Create the report directory
 mkdir -p "$REPORT_DIR"
 
-# Ejecutar Cypress en modo headless (consola)
+# Run Cypress tests in headless mode
 echo "Running Cypress Tests in Headless Mode..."
 npx cypress run --reporter mochawesome --reporter-options reportDir=cypress/reports,overwrite=false
 
-# Fusionar los reportes generados
+# Merge Cypress test reports
     echo "Merging Cypress test reports..."
     npx mochawesome-merge cypress/reports/*.json > cypress/reports/output.json
 
-# Generar el reporte HTML
+# Generate the HTML report
     echo "Generating the HTML report..."
     npx mochawesome-report-generator cypress/reports/output.json
 
